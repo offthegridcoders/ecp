7493386b871bdb1e9bbd4be138aa4133a2718830
Stephen <steveyoung359@gmail.com>
2014-12-26 17:36:59 -0600

    updated

--- js/init.js
+++ js/init.js
@@ -3,4 +3,7 @@
 
 $( document ).ready(function() {
     $('header').css("height", (window.innerHeight - 80)); 
+
+    var newHeaderContentHeight = (window.innerHeight / 2) - ($('.header-content').height()/2);
+    $('.header-content').css("top", newHeaderContentHeight); 
 });
